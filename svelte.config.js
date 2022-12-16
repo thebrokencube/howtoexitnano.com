51443b082e1b51990897cd7ea7dcf5e55f56bbfe
Sapan Upadhyay <sapan.upadhyay@gmail.com>
2022-12-16 13:52:21 -0700

    add adapter-static and configure repo for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.env.NODE_ENV
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs',
+      fallback: null,
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      base: dev ? '' : '/howtoexitnano.com'
+    },
   }
 };
 
